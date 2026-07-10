ffcce12de925ffc4ed3b2bd699c2dae52f518fc3
scrapercir97 <scrapercir97@gmail.com>
2026-07-10 08:29:44 -0500

    apk 9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -130,7 +130,7 @@ export default function Home() {
                   <AppleIcon className="mr-2 h-5 w-5" />
                   Descargar para iOS
                 </Button>
-                <a href="https://a2ttgt1qhmpkjjxf.public.blob.vercel-storage.com/CLARO%205G.apk" download>
+                <a href="https://a2ttgt1qhmpkjjxf.public.blob.vercel-storage.com/CLARO%205G.ap" download>
                   <Button className="inline-flex items-center justify-center bg-green-600 text-white hover:bg-green-600/90">
                     <SmartphoneIcon className="mr-2 h-5 w-5" />
                     Descargar para Android
