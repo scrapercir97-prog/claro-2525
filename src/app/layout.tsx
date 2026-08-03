281c92bc072e3cc6492714eabcbd8db8e384d1f6
scrapercir97 <scrapercir97@gmail.com>
2026-08-03 09:03:19 -0500

    apk 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 // ✅ URL ABSOLUTA - Cambia solo si tu dominio es diferente
-const SITE_URL = "https://www.red-5g-clarofull.online/";
+const SITE_URL = "https://www.5g-app-fullclaro.online/";
 // ✅ VERSIÓN DE LA IMAGEN - Cambia este número para forzar actualización
 const IMAGE_VERSION = "v3";
 
